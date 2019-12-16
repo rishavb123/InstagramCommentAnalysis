2fe0b262f276753e94fa28f289179571f9e6300e
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-12-16 01:53:00 -0500

    Inital Commit

---
+++ analyze.py
@@ -0,0 +1,35 @@
+from instaloader import Instaloader, Post
+from textblob import TextBlob
+import time
+import sys
+
+L = Instaloader()
+
+url = input("Paste the url: ")
+
+code = url.split("/")[len(url.split("/")) - 2]
+
+post = None
+
+try:
+    post = Post.from_shortcode(L.context, code)
+except:
+    L.interactive_login(input("Enter Instagram username: ")) 
+    try:
+        post = Post.from_shortcode(L.context, code)
+    except:
+        print("Post Not Found")
+        sys.exit()
+
+total = 0
+count = 0
+
+for c in post.get_comments():
+    c = c.text
+    blob = TextBlob(c)
+
+    if blob.sentiment.subjectivity > 0.2:
+        total += blob.sentiment.polarity
+        count += 1
+
+print("Average Polarity:", total / count)
