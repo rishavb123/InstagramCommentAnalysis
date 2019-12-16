ef3094f35439069b0d7459de7cb56da38e527460
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-12-16 02:04:13 -0500

    negative post url

--- basic.py
+++ basic.py
@@ -4,6 +4,7 @@ from textblob import TextBlob
 L = Instaloader()
 
 url = input("Paste the url: ")
+# Negative Post: https://www.instagram.com/p/BsbFhcHFvha/
 
 code = url.split("/")[len(url.split("/")) - 2]
 
